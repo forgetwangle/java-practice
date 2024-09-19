1b71824bd0a4ecc177eaa1fc2a0d09f2f53e4d1f
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-09-20 01:38:12 +0800

    Update 1.java

--- 240920/1.java
+++ 240920/1.java
@@ -1,1 +1,28 @@
+package yszl;
 
+public class ngzs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		int u=32;
+		int v=26;
+		while(v!=0) {
+			int temp=u%v;
+			u=v;
+			v=temp;
+		}
+		System.out.println(u);
+
+	}
+
+}
+/*
+yszl
+  JRE System Library
+  src
+    yszl
+      ngzs.java
+    module-info.java
+      yszl
+*/
