7c0a54ca6cca89356f45c7c1e18f7a9e0715fdd2
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-10-28 05:44:45 +0800

    Update 082.java

--- 240920/082.java
+++ 240920/082.java
@@ -18,7 +18,7 @@ public class Hello {
 			buf[i]=(byte)i;
 		}
 		try {
-			DataOutputStream out=new DataOutputStream(
+			DataOutputStream out=new DataOutputStream(//一个DataOutputStream对象是建立在一个别的流基础上的
 					new BufferedOutputStream(
 							new FileOutputStream("a.dat")));//将基本数据类型以二进制的形式写到文件里去
 //			int i=0xcafebabe;
